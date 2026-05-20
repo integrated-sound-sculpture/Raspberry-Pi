67e714a57b17a7b7b33ca7fcc159f0eb43cb538b
Jacob Schoones <J.E.Schoones@students.tudelft.nl>
2026-05-20 21:59:52 +0200

    comments in main om te kunnen testen

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
+
 #include <pulse_cnt.h>
 #include <audio.h>
-
+#include <sine_gen.h>
 
 void setup()
 {
     Serial.begin(115200);
-    // init_frequencyMeter();
+    //init_frequencyMeter();
+    //init_gen_wave(300,8,SINE_WAVE);
     audio.setup();
     audio.enableAudioBeacon();
 }
 
 void loop()
 {
-    // int freq = freq_measurement();
+   // int freq = freq_measurement();
+   // gen_wave(1200,8,SINE_WAVE);
 }
 
