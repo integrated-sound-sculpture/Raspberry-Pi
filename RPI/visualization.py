85d166fa8da4ffef890b58fc58e832978890a0bb
Tprklaver <167996565+Tprklaver@users.noreply.github.com>
2026-06-15 13:02:53 +0200

    Update visualization.py

--- RPI/visualization.py
+++ RPI/visualization.py
@@ -6,11 +6,11 @@ from scipy.io import wavfile
 import numpy as np
 import pyqtgraph as pg
 import sys
-# import sounddevice as sd
+import sounddevice as sd
 import threading
 from scipy.fft import fft, fftfreq
 from scipy.signal import spectrogram
-# import soundfile as sf
+import soundfile as sf
 import socket
 from pyqtgraph.Qt import QtCore, QtWidgets, QtGui
 
@@ -21,58 +21,40 @@ sock.setblocking(False)
 
 sample_rate = 44100
 
-# def main():
-#     while True:
-#         data, addr = sock.recvfrom(2048)
-#         samples = np.frombuffer(data, dtype=np.int16)
-#         sample_rate = 44100
-#         print(samples)
-#     return 0
+app = QtWidgets.QApplication(sys.argv)                                          #start the application                                    #normalize samples to -1.0 to 1.0 
 
-# if _name_ == '_main_':
-#     import sys
-#     main()
+chunk_size = 4096                                             
+time = np.arange(chunk_size) / sample_rate                                      #time array for one chunk, used for plotting the waveform
+chunk = np.zeros(chunk_size)                                                    #audio data chunk
+current_chunk = chunk.copy()                                                    #copy of the current chunk, used as a register
 
+hann_window = np.hanning(chunk_size)                                                   #hann window for the spectrogram, to reduce spectral leakage
+windowed_chunk = chunk * hann_window                                                   #apply the window to the chunk before performing the FFT
 
-app = QtWidgets.QApplication(sys.argv)                              #start the application
+fft_values = np.abs(np.array(fft(windowed_chunk)))[:chunk_size//2]                       #FFT values for the chunk // 2 to only take positive frequencies
+fft_freqs = fftfreq(chunk_size, 1/sample_rate)[:chunk_size//2]                  #frequency corresponding to fft_values
 
-#pyqtgraph.examples.run()
-# samples, sample_rate = sf.read(r"C:\BAP\Theremin_sci-fi.wav", dtype='float32')        #read out the wav file
-
-# if samples.ndim > 1:                                                #in the case of stereo, take one channel
-#     samples = samples[:, 0]
-
-# samples = samples.astype(np.float32)                                #convert to float32 for processing, int will cut off values too much
-# samples = samples / np.max(np.abs(samples))                         #normalize samples to -1.0 to 1.0 
-
-chunk_size = 2048                                             
-time = np.arange(chunk_size) / sample_rate                          #time array for one chunk, used for plotting the waveform
-chunk = np.zeros(chunk_size)                                   #audio data chunk
-current_chunk = chunk.copy()                                        #copy of the current chunk, used as a register
-
-fft_values = np.abs(np.array(fft(chunk)))[:chunk_size//2]           #FFT values for the chunk // 2 to only take positive frequencies
-fft_freqs = fftfreq(chunk_size, 1/sample_rate)[:chunk_size//2]      #frequency corresponding to fft_values
-
-win = QtWidgets.QWidget()                                           #window setup
+win = QtWidgets.QWidget()                                                       #window setup
 win.setWindowTitle('Audio Visualization')
 layout = QtWidgets.QVBoxLayout()
 win.setLayout(layout)
 
-selector = QtWidgets.QComboBox()                                    #create dropdown selector
+selector = QtWidgets.QComboBox()                                                #create dropdown selector
 selector.addItems(['All', 'Sine Wave', 'FFT', 'Spectrogram','Notes'])
 layout.addWidget(selector)
+last_selector = 0
 
-p1_ymax = 0.1                                                       #initial y-axis max's, will be adjucted in update()
+p1_ymax = 0.1                                                                   #initial y-axis max's, will be adjucted in update()
 p2_ymax = 0.1 
 p3_fft_ymax = 0.1  
 
-f_test, t_test, Sxx_test = spectrogram(chunk, fs=sample_rate, nperseg=256, noverlap=128)   #only f-test is used, nperseg is window size, noverlap is amount op points that overlap between windows
-n_freqs = len(f_test)                                               # 256 / 2 + 1 = 129, single sided, including the zero
-waterfall_buffer = np.zeros((n_freqs, 50)) - 80                     # buffer maken voor de waterfall plot, -80 om de eerste frames donker te maken
-log_freqs = np.logspace(np.log10(20), np.log10(sample_rate//2), n_freqs) #log frequencies van 20 tot nyquist
+f_test, t_test, Sxx_test = spectrogram(chunk, fs=sample_rate, nperseg=512, noverlap=256)   #only f-test is used, nperseg is window size, noverlap is amount op points that overlap between windows
+n_freqs = len(f_test)                                                           # 512 / 2 + 1 = 257, single sided, including the zero
+waterfall_buffer = np.zeros((n_freqs, 200)) - 80                                 # Making a bbuffer for the waterfall spectrogram, -80 to make the first few frames dark
+log_freqs = np.logspace(np.log10(20), np.log10(sample_rate//2), n_freqs)        #log frequencies from 20 untill nyquist frequency
 
-freq_labels = [20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000]
-tick_positions = [(np.log10(f), f'{f} Hz') for f in freq_labels]              #afstand van fft_freqs tot de frequenties
+freq_labels = [20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000]           # frequencie labels.
+tick_positions = [(np.log10(f), f'{f} Hz') for f in freq_labels]                #afstand van fft_freqs tot de frequenties
 tick_positions_log = [(int(np.argmin(np.abs(log_freqs - f))), f'{f}') for f in freq_labels]  #afstand van fft_freqs tot de frequenties
 
 note_labels = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'] #Note labels, for some reason it starts with C
@@ -80,12 +62,14 @@ midi_min = 21
 midi_max = 108
 midi_freqs = 440 * 2 ** ((np.arange(midi_min, midi_max + 1) - 69) / 12)  #freq die bij elke midi toon hoort
 
-amplitude_threshold = 2  #drempel voor het tonen van de gedetecteerde noot, om ruis te voorkomen
+amplitude_threshold = 5  #drempel voor het tonen van de gedetecteerde noot, om ruis te voorkomen
 
 p1 = pg.PlotWidget(title= 'sine')                                   #plot waveform
 p1.setLabel('bottom', 'Time', units='s')
 p1.setLabel('left', 'Amplitude')
 graph1 = p1.plot(time.flatten(), chunk.astype(float).flatten())     #flatten to use with pyqtgraph
+p1.setXRange(0,time[-1])
+p1.enableAutoRange(axis='x', enable=False)
 layout.addWidget(p1)
 
 p2 = pg.PlotWidget(title= 'FFT')                                    #plot FFT     
@@ -105,15 +89,12 @@ p3_packet.setLayout(p3_layout)                                          #put the
 p3_fft = pg.PlotWidget(title= 'FFT')                                    #plot FFT of the packet
 p3_fft.setLabel('left', 'Magnitude')
 p3_fft.getAxis('left').setWidth(60)                                     #a constant width for y axis to ensure both y axis are the same
-#p3_fft.setLabel('bottom', 'Frequency', units='Hz')
 graph3_fft = p3_fft.plot(fft_freqs.flatten(), fft_values.flatten())     #flatten to use with pyqtgraph
-#p3_fft.getAxis('bottom').setTicks([tick_positions])                       #set frequency labels on the x-axis
-#p3_fft.setXRange(20, sample_rate//2)                                              #limit x-axis
 p3_fft.getAxis('bottom').setStyle(showValues=False)                         #hide x-axis labels for the fft plot in the packet becasue they are shown in the spectrogram
 p3_fft.setYRange(0, p3_fft_ymax)                                              #initial y-axis range
 p3_layout.addWidget(p3_fft)
 
-p3 = pg.PlotWidget()#title= 'Spectrogram')                            #plot spectrogram
+p3 = pg.PlotWidget()                                                    #plot spectrogram
 p3.setLabel('left', 'Time (frames)')
 p3.setLabel('bottom', 'Frequency', units='Hz')
 p3.getAxis('left').setWidth(60)
@@ -133,6 +114,7 @@ p4.setYRange(0, 1)
 p4.setXRange(0, 88)
 p4.getAxis('bottom').setStyle(showValues=False)
 p4.getAxis('left').setStyle(showValues=False)
+p4.hide()
 
 white_keys = []
 black_keys = []
@@ -176,7 +158,7 @@ def on_select(index):                                               #function to
         p1.show()
         p2.hide()
         p3_packet.show()
-        p4.show()
+        p4.hide()
     elif index == 1:
         p1.show()
         p2.hide()
@@ -200,59 +182,36 @@ def on_select(index):                                               #function to
 
 selector.currentIndexChanged.connect(on_select)
 
-# audio_running = True
-# def audio_thread():                                                #Test thread fit audio to the plots
-#     global current_chunk
-#     audio_pos = 0
-#     with sd.OutputStream(samplerate=sample_rate, channels=1) as stream:
-#         while audio_running:
-#             if audio_pos + chunk_size > len(samples):
-#                 audio_pos = 0
-#             chunk = samples[audio_pos:audio_pos + chunk_size]
-#             current_chunk = chunk.copy()
-#             if not audio_running:
-#                 break
-#             stream.write(chunk)        
-#             audio_pos += chunk_size
-
-# thread = threading.Thread(target=audio_thread, daemon=True)
-# thread.start()
-
-def on_app_quit():                                                  #fix the error of closing while thread is still running
-    # global audio_running
-    # audio_running = False
-    # thread.join(timeout=1.0)
-    pass
-
-app.aboutToQuit.connect(on_app_quit)                                # Connect the cleanup function to the application's quit signal
-
 smooth_midi = [69.0]
 
 def update():                                                       #update function for the plots, called by a timer
-    global waterfall_buffer, chunk_size, p1_ymax, p2_ymax, p3_fft_ymax
+    global waterfall_buffer, chunk, p1_ymax, p2_ymax, p3_fft_ymax, last_selector
 
     data, addr = sock.recvfrom(chunk_size * 2)
     chunk = np.frombuffer(data, dtype=np.int16)
 
+    view_selector = chunk[0]
+    samples = data[1:]
 
+    chunk = np.frombuffer(chunk, dtype=np.int16)
     chunk = chunk / np.max(np.abs(chunk))
 
-    if chunk is None:
-        return
-    # chunk = current_chunk.copy()                                    #making use of the register
+    windowed_chunk = chunk * hann_window
+    if view_selector != last_selector:
+        selector.setCurrentIndex({0:1,1:3,2:4}.get(view_selector, 0))  # Update the selector based on the received data
+        last_selector = view_selector
 
-    fft_values = np.abs(np.array(fft(chunk)))[:chunk_size//2]       #perform fft, put it in an array and take the absolute value, for only positive frequencies
+
+    fft_values = np.abs(np.array(fft(windowed_chunk)))[:chunk_size//2]       #perform fft, put it in an array and take the absolute value, for only positive frequencies
     fft_freqs = fftfreq(chunk_size, 1/sample_rate)[:chunk_size//2]  #get corresponding frequencies for the fft values
     fft_values_log = np.interp(log_freqs, fft_freqs, fft_values)    #interpolate the fft values to match the log frequency bins
 
-    f,t,Sxx = spectrogram(chunk, fs=sample_rate, nperseg =512, noverlap=256) #make spectrogram
+    f,t,Sxx = spectrogram(chunk, fs=sample_rate, nperseg =512, noverlap=256, window ='hann') #make spectrogram
     magnitude_db_spec = 10 * np.log10(Sxx + 1e-10)                  #convert magnitude to logaritmic, avoid log(0)
     col = magnitude_db_spec.mean(axis=1)                            #average over time to get a single column for the waterfall plot    
     col_log = np.interp(log_freqs, f, col)                          #interpolate to match the log frequency bins
-
     graph1.setData(time.flatten(), chunk.astype(float).flatten())   #update waveform plot
     graph2.setData(fft_freqs.flatten(), fft_values.flatten())       #update FFT plot
-    #graph3_fft.setData(log_freqs, fft_values_log)   #update FFT plot in the spectrogram packet
     graph3_fft.setData(np.arange(len(log_freqs)), fft_values_log)
     waterfall_buffer = np.roll(waterfall_buffer, -1, axis=1)        #shift buffer to the left
     waterfall_buffer[:, -1] = col_log                               #add new column to the right of the buffer  
@@ -270,9 +229,22 @@ def update():                                                       #update func
 
     valid_freq = fft_freqs > 20
     if np.any(valid_freq):
-        peak_idx = np.argmax(fft_values[valid_freq])                #find the index of the peak in the FFT
+        valid_fft = fft_values[valid_freq]
+        valid_ffreqs = fft_freqs[valid_freq]
+        peak_idx = np.argmax(valid_fft)                #find the index of the peak in the FFT
         peak_freq = fft_freqs[valid_freq][peak_idx]                 #get the corresponding frequency
-        peak_magnitude = fft_values[valid_freq][peak_idx]           #get the magnitude of the peak
+        peak_magnitude = valid_fft[peak_idx]           #get the magnitude of the peak
+        
+        if 0 < peak_idx < len(valid_fft) -1:
+            left = valid_fft[peak_idx-1]
+            middle = valid_fft[peak_idx]
+            right = valid_fft[peak_idx+1]
+            correction = 0.5*(left-right)/(left-2*middle+right)
+            bin_width = fft_freqs[1] - fft_freqs[0]
+            peak_freq = valid_ffreqs[peak_idx] + correction * bin_width
+        else:
+            peak_freq = valid_ffreqs[peak_idx]
+
         midi_note = 69 + 12 * np.log2(max(peak_freq / 440, 1e-10))  #convert frequency to MIDI note number
 
         center = round(midi_note)                                   #get the nearest MIDI note number to the smoothed value
@@ -293,7 +265,7 @@ def update():                                                       #update func
 
 timer = QtCore.QTimer()                                             #timer to call the update function every 50 ms
 timer.timeout.connect(update)
-timer.start(5)
+timer.start(20)
 
 win.showMaximized()                                                 #show the window fullscreen
 win.show()
