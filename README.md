ecd23dc5b5dd68cb7d60c137ef37e4fabe0d8067
IngeSchmidt <100145761+IngeSchmidt@users.noreply.github.com>
2026-07-02 15:23:03 +0200

    Raspberry pi code in one place from fork

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ISS_SP
-Signal processing code for the BSc End Project: Interactive Sound Sculpture 
+# Raspberry Pi
+Sample generation and LED control cpp code and visualisation python code for the Raspberry Pi side of the Integrate Sound Sculpture 
--- RPI/delay.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-samples = pd.read_csv('RPI/delay.csv')
-
-print("Parameter aquisition \t\t\t Min:", samples['Parameter'].min(), "\t\tMax:", samples['Parameter'].max(), "\tMean:", samples['Parameter'].mean())
-print("Wave generation \t\t\t Min:", samples['Wave'].min(), "\t\tMax:", samples['Wave'].max(), "\tMean:", samples['Wave'].mean())
-print("Sending buffer to audio \t\t Min:", samples['Buffer'].min(), "\tMax:", samples['Buffer'].max(), "\tMean:", samples['Buffer'].mean())
-print("Sending buffer to visualisation \t Min:", samples['Visualisation'].min(), "\t\tMax:", samples['Visualisation'].max(), "\tMean:", samples['Visualisation'].mean())
